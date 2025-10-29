f19dc9cd5b1cdc56db7b8c7ac465e3d8427f3db7
Wagner-rosa-dev <wagner.rosa.dev@gmail.com>
2025-10-29 08:30:34 -0300

    Minha primeira lista de exercicios feito para adicionar no git - A lista quem fez foi meu professor da faculdade

---
+++ lista1.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+/* Primeiro exercicio
+int main() {
+    string nome;
+    cout << "Qual seu nome completo? " << endl;
+    getline(cin, nome);
+
+    cout << "Boa tarde!!! " <<  nome;
+
+    return 0;
+}
+*/
+/*
+int main() {
+    int soma, num1, num2;
+
+    cout << "Digite o primeiro numero: " << endl;
+    cin >> num1;
+    cout << "Digite o segundo numero: " << endl;
+    cin >> num2;
+
+    soma = num1 + num2;
+
+    cout << "A soma desses dois numeros é: " << soma<< endl;
+
+    return 0;
+}
+*/
+/*
+int main() {
+    float valor, desconto, resultado;
+
+    cout << "Calculo de desconto!!!!" << endl;
+    cout << "Digite um valor para o seu produto: " << endl;
+    cin >> valor;
+    cout << "Agora qual o valor do desconto: " << endl;
+    cin >> desconto;
+
+    resultado = valor - (valor * (desconto/100));
+
+    cout << "o valor colocado foi: " << valor << endl;
+    cout << "o valor de desconto: " << desconto << "%" << endl;
+    cout << "o valor após o desconto é: " << resultado<< endl;
+
+    return 0;
+}
+*/
+/*
+int main() {
+    int horario;
+
+    cout << "digite somente a hora atual(ignorando os minutos): " << endl;
+    cin >> horario;
+
+    if (horario >= 6 && horario < 12) {
+        cout << "Seu horário é " << horario << " da manhã" << endl;
+        cout << "Bom dia!!!!!"<< endl;
+    } else if(horario >= 12 && horario <18) {
+        cout << "Seu horário é " << horario << " da tarde" << endl;
+        cout << "Boa tarde!!!"<< endl;
+    } else if (horario >= 18 && horario < 6) {
+        cout << "Seu horário é " << horario << " da noite" << endl;
+        cout << "Boa noite!!!"<< endl;
+    } else if (horario < 0 || horario > 24) {
+        cout << "Valor inválido" << endl;
+    }
+
+    return 0;
+}
+*/
+/*
+int main() {
+    float nota1, nota2, media;
+
+    cout << "Verificação de média!" << endl;
+    cout << "Digite sua primeira nota: " << endl;
+    cin >> nota1;
+    cout << "Digite sua segunda nota: " << endl;
+    cin >> nota2;
+
+    media = (nota1 + nota2) / 2;
+
+    cout << "A média das suas notas é: " << media << endl;
+
+    return 0;
+}
+*/
+/*
+int main() {
+    float nota1, nota2, media;
+
+    cout << "Verificação de média!" << endl;
+    cout << "Digite sua primeira nota: " << endl;
+    cin >> nota1;
+    cout << "Digite sua segunda nota: " << endl;
+    cin >> nota2;
+
+    media = (nota1 + nota2) / 2;
+
+    if (media >= 7){
+        cout << "A média das suas notas é: " << media << endl;
+        cout << "Parabéns você foi aprovado!!!!" << endl;
+    } else {
+        cout << "A média das suas notas é: " << media << endl;
+        cout << "Reprovado - nota insuficiente!" << endl;
+    }
+    return 0;
+}
+*/
+/*
+int main() {
+    int idade;
+
+    cout << "Qual a sua idade ? " << endl;
+    cin >> idade;
+
+    if (idade <= 12) {
+        cout << "Você é criança!" << endl;
+    } else if (idade > 12 && idade <= 18) {
+        cout << "Você é adolescente!" << endl;
+    } else if (idade > 18 && idade <= 59){
+        cout << "Você é adulto(a)!" << endl;
+    } else if (idade > 59) {
+        cout << "Você é Idoso(a)!" << endl;
+    }
+    
+    return 0;
+}
+*/
+
+int main(){
+    
+    
+    
+    
+    return 0;
+}
